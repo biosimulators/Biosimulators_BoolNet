90e02e1da4fa7d7505518d8c6c1e9f982087401a
Jonathan Karr <jonrkarr@gmail.com>
2021-08-17 13:26:58 -0400

    feat: updated to BioSimulators-utils 0.1.104

--- biosimulators_boolnet/__init__.py
+++ biosimulators_boolnet/__init__.py
@@ -1,4 +1,13 @@
+from ._version import __version__
+from .core import exec_sed_task, exec_sedml_docs_in_combine_archive  # noqa: F401
 from .utils import get_boolnet
+
+__all__ = [
+    '__version__',
+    'get_simulator_version',
+    'exec_sed_task',
+    'exec_sedml_docs_in_combine_archive',
+]
 
 
 def get_simulator_version():
